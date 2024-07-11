512b08a11ad03a88389a35fca9c39924e757d257
Johannbulls <johannpez99@gmail.com>
2024-07-11 09:38:08 -0500

    first commit

---
+++ src/test/java/edu/escuelaing/arsw/ASE/app/WebSecurity/WebSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ASE.app.WebSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
